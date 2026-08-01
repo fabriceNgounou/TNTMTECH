890496501d3474138176ff9c1e8d70409b53c679
RUSSELBRANDON <russeldongmo96@gmail.com>
2026-08-01 13:28:56 +0200

    Use Vercel PHP runtime 0.9.0

--- api/index.php
+++ api/index.php
@@ -1,11 +1,27 @@
 <?php
 
+use Illuminate\Contracts\Console\Kernel as ConsoleKernelContract;
 use Illuminate\Contracts\Http\Kernel;
 use Illuminate\Http\Request;
+
+$databasePath = $_SERVER['DB_DATABASE'] ?? $_ENV['DB_DATABASE'] ?? null;
+if ($databasePath && str_starts_with($databasePath, '/tmp') && ! file_exists($databasePath)) {
+    @touch($databasePath);
+}
 
 require __DIR__.'/../vendor/autoload.php';
 
 $app = require_once __DIR__.'/../bootstrap/app.php';
+
+if ($databasePath && str_starts_with($databasePath, '/tmp')) {
+    $consoleKernel = $app->make(ConsoleKernelContract::class);
+    $migrationMarker = '/tmp/.vercel_sqlite_migrated';
+
+    if (! file_exists($migrationMarker)) {
+        $consoleKernel->call('migrate', ['--force' => true]);
+        @touch($migrationMarker);
+    }
+}
 
 $kernel = $app->make(Kernel::class);
 $request = Request::capture();
